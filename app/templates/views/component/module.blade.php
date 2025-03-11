a203aabd49217356f9519da47a38e9ae2345e2eb
thaingocphu0803 <167735819+thaingocphu0803@users.noreply.github.com>
2025-03-11 15:38:16 +0700

    fix(generate module): fix component module template v3

--- app/templates/views/component/module.blade.php
+++ app/templates/views/component/module.blade.php
@@ -1,21 +1,19 @@
             <x-backend.dashboard.nav.module icon='fa-file' :title="__('dashboard.managerObject', ['object' => __('dashboard.{moduleName}')])">
-                @if (Route::has('{moduleName}.index'))
-                    @can('modules', '{moduleName}.index')
-                        <li>
-                            <a class="text-capitalize" href="{{ route('{moduleName}.index') }}">
-                                {{ __('dashboard.{moduleName}') }}
-                            </a>
-                        </li>
-                    @endcan
-                @endif
+                @can('modules', '{moduleName}.index')
+                    <li>
+                        <a class="text-capitalize"
+                            href="{{ Route::has('{moduleName}.index') ? route('{moduleName}.index') : '#' }}">
+                            {{ __('dashboard.{moduleName}') }}
+                        </a>
+                    </li>
+                @endcan
 
-                @if (Route::has('{moduleName}.catalouge.index'))
-                    @can('modules', '{moduleName}.catalouge.index')
-                        <li>
-                            <a class="text-capitalize" href="{{ route('{moduleName}.catalouge.index') }}">
-                                {{ __('dashboard.objectGroup', ['object' => __('dashboard.{moduleName}')]) }}
-                            </a>
-                        </li>
-                    @endcan
-                @endif
+                @can('modules', '{moduleName}.catalouge.index')
+                    <li>
+                        <a class="text-capitalize"
+                            href="{{ Route::has('{moduleName}.catalouge.index') ? route('{moduleName}.catalouge.index') : '#' }}">
+                            {{ __('dashboard.objectGroup', ['object' => __('dashboard.{moduleName}')]) }}
+                        </a>
+                    </li>
+                @endcan
             </x-backend.dashboard.nav.module>
