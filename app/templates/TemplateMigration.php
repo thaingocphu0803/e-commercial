93f4dd654cbaec848e9e8071efe885b6abbb3465
thaingocphu0803 <167735819+thaingocphu0803@users.noreply.github.com>
2025-03-11 14:48:46 +0700

    fix(generate module): add nested v2

--- app/templates/TemplateMigration.php
+++ app/templates/TemplateMigration.php
@@ -3,6 +3,7 @@ use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Kalnoy\Nestedset\NestedSet;
 
 return new class extends Migration
 {
